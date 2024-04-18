425f8510f35ce39bd124daf5042dc19a1ebda492
devnote-dev <devnote.dev75@gmail.com>
2024-04-18 16:19:44 +0100

    fix(crystal): add missing methods & types

--- src/redoc/crystal.cr
+++ src/redoc/crystal.cr
@@ -52,7 +52,7 @@ module Redoc
       getter? abstract : Bool
       getter args : Array(MetaArg)?
       getter args_string : String?
-      getter location : Location
+      getter location : Location?
       getter def : MetaDef
     end
 
@@ -78,6 +78,7 @@ module Redoc
       getter name : String
       getter full_name : String
       getter? abstract : Bool
+      getter superclass : TypeRef?
       getter kind : Kind
       getter ancestors : Array(TypeRef)?
       getter locations : Array(Location)
@@ -86,10 +87,14 @@ module Redoc
       getter? enum : Bool
       getter? alias : Bool
       getter? const : Bool
+      getter doc : String?
+      getter summary : String?
       getter constants : Array(ConstDef)?
       getter included_modules : Array(TypeRef)?
       getter extended_modules : Array(TypeRef)?
+      getter constructors : Array(Def)?
       getter class_methods : Array(Def)?
+      getter instance_methods : Array(Def)?
       getter macros : Array(Def)?
       getter types : Array(Program)?
     end
