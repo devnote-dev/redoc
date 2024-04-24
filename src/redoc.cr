21b356f7bb8eb6f5e918b499c5f35fa9b6f50a80
devnote-dev <devnote.dev75@gmail.com>
2024-04-24 20:11:12 +0100

    feat: add load helper method

--- src/redoc.cr
+++ src/redoc.cr
@@ -6,6 +6,11 @@ module Redoc
   VERSION       = "0.1.0"
   QUERY_PATTERN = /^(?<ns>(?:::)?[A-Z_]{1,}(?:\w+|::)+)?(?:(?<scp>\.|#|\s+)?(?<sym>[a-zA-Z_]{1,}[\w!?=]|[!?<>^+\-~\/*&%|`]|<<|>>|\/\/|\*\*|===?|\[\]=?))?$/
 
+  def self.load(source : String | IO) : Project
+    program = Crystal::Program.from_json source
+    Transformer.transform program
+  end
+
   def self.parse_query?(pattern : String) : {Array(String), String?, Bool}?
     return unless match = QUERY_PATTERN.match pattern
     namespace = match["ns"]?.try(&.split("::", remove_empty: true)) || [] of String
