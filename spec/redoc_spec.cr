c0ebde3d3628c2d25bf672a8082494c2d8c266ee
devnote-dev <devnote.dev75@gmail.com>
2024-10-01 19:05:56 +0100

    refactor(spec): update examples and coverage

--- spec/redoc_spec.cr
+++ spec/redoc_spec.cr
@@ -3,67 +3,79 @@ require "./spec_helper"
 describe Redoc do
   context "query" do
     it "should parse identifiers as symbols" do
-      namespace, symbol, kind = Redoc.parse_query "puts"
+      namespace, symbol, scope = Redoc.parse_query "puts"
       namespace.should be_empty
       symbol.should eq "puts"
-      kind.should eq Redoc::QueryKind::All
-
-      namespace, symbol, kind = Redoc.parse_query "#puts"
-      namespace.should be_empty
-      symbol.should eq "puts"
-      kind.should eq Redoc::QueryKind::Instance
-
-      namespace, symbol, kind = Redoc.parse_query ".puts"
-      namespace.should be_empty
-      symbol.should eq "puts"
-      kind.should eq Redoc::QueryKind::Class
+      scope.should eq Redoc::QueryScope::All
     end
 
     it "should parse a constant as a namespace" do
-      namespace, symbol, kind = Redoc.parse_query "STDOUT"
+      namespace, symbol, scope = Redoc.parse_query "STDOUT"
       namespace.should eq %w[STDOUT]
       symbol.should be_nil
-      kind.should eq Redoc::QueryKind::All
+      scope.should eq Redoc::QueryScope::All
     end
 
     it "should parse constants as namespaces" do
-      namespace, symbol, kind = Redoc.parse_query "IO::Memory"
+      namespace, symbol, scope = Redoc.parse_query "IO::Memory"
       namespace.should eq %w[IO Memory]
       symbol.should be_nil
-      kind.should eq Redoc::QueryKind::All
+      scope.should eq Redoc::QueryScope::All
 
-      namespace, symbol, kind = Redoc.parse_query "::JSON::Any::Type"
+      namespace, symbol, scope = Redoc.parse_query "::JSON::Any::Type"
       namespace.should eq %w[JSON Any Type]
       symbol.should be_nil
-      kind.should eq Redoc::QueryKind::All
+      scope.should eq Redoc::QueryScope::All
     end
 
     it "should parse paths with periods" do
-      namespace, symbol, kind = Redoc.parse_query "IO.puts"
+      namespace, symbol, scope = Redoc.parse_query "IO.puts"
       namespace.should eq %w[IO]
       symbol.should eq "puts"
-      kind.should eq Redoc::QueryKind::Class
+      scope.should eq Redoc::QueryScope::Class
     end
 
     it "should parse paths with hashtags" do
-      namespace, symbol, kind = Redoc.parse_query "Array#size"
+      namespace, symbol, scope = Redoc.parse_query "Array#size"
       namespace.should eq %w[Array]
       symbol.should eq "size"
-      kind.should eq Redoc::QueryKind::Instance
+      scope.should eq Redoc::QueryScope::Instance
 
-      namespace, symbol, kind = Redoc.parse_query "Hash#[]"
+      namespace, symbol, scope = Redoc.parse_query "Hash#[]"
       namespace.should eq %w[Hash]
       symbol.should eq "[]"
-      kind.should eq Redoc::QueryKind::Instance
+      scope.should eq Redoc::QueryScope::Instance
     end
 
     it "disallows invalid identifiers" do
-      expect_raises(Error) do
-        Redoc.parse_query "::puts"
+      expect_raises(Redoc::Error) do
+        Redoc.parse_query ".puts"
       end
 
-      expect_raises(Error) do
+      expect_raises(Redoc::Error) do
+        Redoc.parse_query "#puts"
+      end
+
+      expect_raises(Redoc::Error) do
         Redoc.parse_query "puts::"
+      end
+
+      expect_raises(Redoc::Error) do
+        Redoc.parse_query "to_s.nil?"
+      end
+
+      expect_raises(Redoc::Error) do
+        Redoc.parse_query "IO.Memory"
+      end
+
+      expect_raises(Redoc::Error) do
+        Redoc.parse_query "JSON#Any"
+      end
+    end
+
+    it "parses operator tokens" do
+      %w[+ - * ** / // % | ! !~ & &+ &- &* ^ ` ~ =~ == === < << <= <=> >> > >= [] []? []=].each do |op|
+        Redoc::METHOD_PATTERN.matches?(op).should be_true
       end
     end
   end
